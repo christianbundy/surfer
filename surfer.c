d8e32d25a8079840aa51227a8515337bdd095a1e
Christian Bundy <christianbundy@fraction.io>
2018-07-19 17:38:00 -0700

    Auto-select URI

--- surfer.c
+++ surfer.c
@@ -484,6 +484,7 @@ keyboard(GtkWidget *widget,GdkEvent *event, Client *c,  gpointer data) {
                     url = webkit_web_view_get_uri(WEBKIT_WEB_VIEW(c->webView));
                     gtk_entry_set_text(GTK_ENTRY(c->entry_open), url);
                         c->o = 1;
+												gtk_widget_grab_focus(GTK_WIDGET(c->entry_open));
 		
                     } else {
                      gtk_widget_hide(c->box_open);
